a1004a1f46f7b0aff866ddedd31d2b895ff14520
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2025-09-11 08:41:57 -0700

    fix: update wasm bindgen; rm transpiled JS

--- crates/bitwarden-wasm/build.sh
+++ crates/bitwarden-wasm/build.sh
@@ -1,23 +1,24 @@
+#!/usr/bin/env bash
+
 # Move to the root of the repository
 cd "$(dirname "$0")"
 cd ../../
 
 if [ "$1" != "-r" ]; then
-  # Dev
-  cargo build -p bitwarden-wasm --target wasm32-unknown-unknown
-  wasm-bindgen --target bundler --out-dir languages/js/wasm ./target/wasm32-unknown-unknown/debug/bitwarden_wasm.wasm
-  wasm-bindgen --target nodejs --out-dir languages/js/wasm/node ./target/wasm32-unknown-unknown/debug/bitwarden_wasm.wasm
+  echo "Building in debug mode"
+  RELEASE_FLAG=""
+  BUILD_DIR="debug"
 else
-  # Release
-  cargo build -p bitwarden-wasm --target wasm32-unknown-unknown --release
-  wasm-bindgen --target bundler --out-dir languages/js/wasm ./target/wasm32-unknown-unknown/release/bitwarden_wasm.wasm
-  wasm-bindgen --target nodejs --out-dir languages/js/wasm/node ./target/wasm32-unknown-unknown/release/bitwarden_wasm.wasm
+  echo "Building in release mode"
+  RELEASE_FLAG="--release"
+  BUILD_DIR="release"
 fi
+
+# Build
+cargo build -p bitwarden-wasm --target wasm32-unknown-unknown ${RELEASE_FLAG}
+wasm-bindgen --target bundler --out-dir languages/js/wasm ./target/wasm32-unknown-unknown/${BUILD_DIR}/bitwarden_wasm.wasm
+wasm-bindgen --target nodejs --out-dir languages/js/wasm/node ./target/wasm32-unknown-unknown/${BUILD_DIR}/bitwarden_wasm.wasm
 
 # Optimize size
 wasm-opt -Os ./languages/js/wasm/bitwarden_wasm_bg.wasm -o ./languages/js/wasm/bitwarden_wasm_bg.wasm
 wasm-opt -Os ./languages/js/wasm/node/bitwarden_wasm_bg.wasm -o ./languages/js/wasm/node/bitwarden_wasm_bg.wasm
-
-# Transpile to JS
-wasm2js ./languages/js/wasm/bitwarden_wasm_bg.wasm -o ./languages/js/wasm/bitwarden_wasm_bg.wasm.js
-npx terser ./languages/js/wasm/bitwarden_wasm_bg.wasm.js -o ./languages/js/wasm/bitwarden_wasm_bg.wasm.js
